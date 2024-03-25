aca316ebe30e1bdcae731c7927a9634d62e5c7b2
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-25 21:05:40 +0100

    feat : add props for search word in result component

--- src/components/ShowResult.tsx
+++ src/components/ShowResult.tsx
@@ -2,16 +2,18 @@ import { IMovie } from "../models/IMovie";
 
 interface IShowResultProps {
   movies: IMovie[];
+  searchWord: string;
 }
 
-export const ShowResult = ({ movies }: IShowResultProps) => {
+export const ShowResult = ({ movies, searchWord }: IShowResultProps) => {
   return (
-    <section className="py-10">
+    <section className="p-10">
+        <h1>Result for: {searchWord}</h1>
       <div className="movies flex flex-wrap justify-center gap-8 before:max-w-72 before:w-full before:order-1 after:max-w-72 after:w-full">
         {movies.map((movie) => (
           <div
             key={movie.imdbID}
-            className="max-w-72 flex flex-col items-center justify-end gap-5 pt-8"
+            className="max-w-72 flex flex-col items-center justify-between gap-5 pt-8"
           >
             <img src={movie.Poster} alt={movie.Title} className="rounded-xl" />
             <h3 className="text-lg font-medium text-center">{movie.Title}</h3>
--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -7,8 +7,10 @@ import { ShowResult } from "../components/ShowResult";
 
 export const MovieSearch = () => {
   const [movies, setMovies] = useState<IMovie[]>([]);
+  const [searchWord, setSearchWord] = useState("");
 
   const searchMovies = async (searchWord: string) => {
+    setSearchWord(searchWord)
     const response = await axios.get<IOmdbResponse>(
       `http://www.omdbapi.com/?apikey=${
         import.meta.env.VITE_OMDB_API_KEY
@@ -20,7 +22,7 @@ export const MovieSearch = () => {
   return (
     <div className="min-h-screen">
       <SearchForm search={searchMovies} />
-      <ShowResult movies={movies} />
+      <ShowResult movies={movies} searchWord={searchWord} />
     </div>
   );
 };
