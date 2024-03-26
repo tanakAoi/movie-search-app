a11d541f6b891d8cb77b37ae8a2458834b7cfef1
Aoi Tanaka <aoitanaka.0523@gmail.com>
2024-03-26 21:27:43 +0100

    feat : change details for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,14 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <header>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-zinc-300">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Movie Search</a>
+          <NavLink className="btn btn-ghost text-xl" to={"/"}>
+            Movie Search
+          </NavLink>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 flex gap-2">
             <li>
               <NavLink to={"/"}>Home</NavLink>
             </li>
